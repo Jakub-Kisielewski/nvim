3f251f4f38f6d6d75f53b5391766256d2dc34b85
Jakub Kisielewski <Jakub.Kisielewski@warwick.ac.uk>
2025-12-22 12:36:16 +0000

    ctrl-v and c to system clipboard

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,4 +2,5 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-
+-- clipboard
+vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register
