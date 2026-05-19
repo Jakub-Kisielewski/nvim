4202b4e1ef234b66747246bb163944f2aac95aad
Jakub Kisielewski <kisielewskij66@gmail.com>
2026-05-19 19:02:24 +0100

    toggleable transparency

--- plugin/catppuccin.lua
+++ plugin/catppuccin.lua
@@ -1,2 +1,34 @@
--- colourscheme
 vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } }
+
+-- 1. Create a variable to remember your current state
+local is_transparent = false
+
+-- 2. Wrap the setup and colorscheme application in a function
+local function apply_catppuccin()
+    require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = is_transparent,
+    })
+    -- The standard command is "catppuccin", not "catppuccin-nvim"
+    vim.cmd.colorscheme("catppuccin")
+end
+
+-- 3. Initial load (using a safe pcall so it doesn't crash on fresh installs)
+local ok, _ = pcall(require, "catppuccin")
+if ok then
+    apply_catppuccin()
+else
+    vim.cmd("packadd catppuccin")
+end
+
+-- 4. Create the keybinding to toggle the state
+vim.keymap.set("n", "<leader>tt", function()
+    -- Flip the true/false state
+    is_transparent = not is_transparent
+    
+    -- Re-run the setup and redraw the colors
+    apply_catppuccin()
+    
+    -- Optional: show a little message at the bottom of the screen
+    print("Background transparency: " .. tostring(is_transparent))
+end, { desc = "Toggle Catppuccin Transparency" })
